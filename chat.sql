dd8ca932ffc91d93651acdda76abaab7a52937b0
AshleyDudek <adudek@mail.umw.edu>
2015-03-31 13:54:54 -0400

    Update chat.sql

--- chat.sql
+++ chat.sql
@@ -13,9 +13,9 @@ CREATE TABLE IF NOT EXISTS users (
   PRIMARY KEY (userid)
 )  ;
 
-INSERT INTO users VALUES (1, 'ashley', 'ugh');
-INSERT INTO users VALUES (2, 'tyler', 'gross');
-INSERT INTO users VALUES (3, 'savannah', 'yay');
+INSERT INTO users (username, password) VALUES ('ashley', 'ugh');
+INSERT INTO users (username, password) VALUES ('tyler', 'gross');
+INSERT INTO users (username, password) VALUES ('savannah', 'yay');
 
 -- CHATROOMS
 
@@ -23,8 +23,34 @@ DROP TABLE IF EXISTS chatrooms;
 CREATE TABLE IF NOT EXISTS chatrooms (
   roomid serial,
   name varchar(12) NOT NULL,
+  chatpassword varchar(126) NOT NULL,
   PRIMARY KEY (roomid)
 )  ;
+
+INSERT INTO chatrooms (name, chatpassword) VALUES ('Friendship', 'not tyler');
+INSERT INTO chatrooms (name, chatpassword) VALUES ('Video Games', 'fpl');
+INSERT INTO chatrooms (name, chatpassword) VALUES ('Class Stuff', 'too many cooks');
+INSERT INTO chatrooms (name, chatpassword) VALUES ('Tyler Sucks', 'all day');
+
+-- ROOM/USER SUBSCRIPTION JUNCTION
+
+DROP TABLE IF EXISTS sub;
+CREATE TABLE IF NOT EXISTS sub (
+  roomid int,
+  userid int
+);
+
+INSERT INTO sub VALUES (1, 1);
+INSERT INTO sub VALUES (1, 2);
+INSERT INTO sub VALUES (1, 3);
+INSERT INTO sub VALUES (2, 1);
+INSERT INTO sub VALUES (2, 2);
+INSERT INTO sub VALUES (2, 3);
+INSERT INTO sub VALUES (3, 1);
+INSERT INTO sub VALUES (3, 2);
+INSERT INTO sub VALUES (3, 3);
+INSERT INTO sub VALUES (4, 1);
+INSERT INTO sub VALUES (4, 3);
   
 
 -- MESSAGES
@@ -33,11 +59,20 @@ DROP TABLE IF EXISTS messages;
 CREATE TABLE IF NOT EXISTS messages (
   messageid serial,
   text varchar(140),
-  userperson varchar(12) NOT NULL,
+  userid int NOT NULL,
+  roomid int NOT NULL,
   PRIMARY KEY (messageid)
 )  ;
 
-INSERT INTO messages VALUES (1, 'hey friends', 'ashley');
-INSERT INTO messages VALUES (2, 'ugh you', 'tyler');
-INSERT INTO messages VALUES (3, 'i like metroid', 'savannah');
-INSERT INTO messages VALUES (4, 'yayyy', 'ashley');
+INSERT INTO messages (text, userid, roomid) VALUES ('hey friends', 1, 1);
+INSERT INTO messages (text, userid, roomid) VALUES ('ugh you', 2, 1);
+INSERT INTO messages (text, userid, roomid) VALUES ('i like metroid', 3, 1);
+INSERT INTO messages (text, userid, roomid) VALUES ('yayyy', 1, 1);
+INSERT INTO messages (text, userid, roomid) VALUES ('metroid???', 3, 2);
+INSERT INTO messages (text, userid, roomid) VALUES ('yes', 1, 2);
+INSERT INTO messages (text, userid, roomid) VALUES ('no', 2, 2);
+INSERT INTO messages (text, userid, roomid) VALUES ('when is sprint 2 due?', 2, 3);
+INSERT INTO messages (text, userid, roomid) VALUES ('now', 1, 3);
+INSERT INTO messages (text, userid, roomid) VALUES ('never', 3, 3);
+INSERT INTO messages (text, userid, roomid) VALUES ('tyler sucks', 1, 4);
+INSERT INTO messages (text, userid, roomid) VALUES ('i know', 3, 4);
